6afded12f33ac1bda60adccc0245f55f9f612897
Jamkris <dltmdgus1412@gmail.com>
2025-04-10 14:06:14 +0900

    fix :: 커스텀 색상 및 폰트 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,32 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/components/useColorScheme';
-
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@styles/theme';
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+	const [fontsLoaded] = useFonts({
+		PretendardBold: require('@/assets/fonts/Pretendard-Bold.otf'),
+		PretendardSemiBold: require('@/assets/fonts/Pretendard-SemiBold.otf'),
+		PretendardMedium: require('@/assets/fonts/Pretendard-Medium.otf'),
+		PretendardRegular: require('@/assets/fonts/Pretendard-Regular.otf'),
+	});
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	if (!fontsLoaded) return null;
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Stack>
+				<Stack.Screen
+					name='index'
+					options={{
+						title: '',
+						headerShadowVisible: false,
+						headerStyle: {
+							backgroundColor: '#FFF',
+						},
+					}}
+				/>
+			</Stack>
+		</ThemeProvider>
+	);
 }
