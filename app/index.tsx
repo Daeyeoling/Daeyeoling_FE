0fbaebd8646ddf4658b8b8b6c8e0e7e3e211ab5b
Jamkris <dltmdgus1412@gmail.com>
2025-04-11 08:51:58 +0900

    add :: 제품 리스트 페이지 완성

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,85 @@
-import { Stack, useNavigation, useRouter } from 'expo-router';
-import { Text, TouchableOpacity } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { FlatList, Image, Pressable } from 'react-native';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 import * as s from '@styles/index';
 
+const DATA = Array(10).fill({
+	title: '에어팟 프로',
+	location: '군자동',
+	date: '3일 전',
+	price: '220,000원',
+	comments: 3,
+	likes: 11,
+	image:
+		'https://1801889e95b1f9bf.kinxzone.com/webfile/product/9/9134/s8rg44227936.jpeg',
+});
+
 export default function Index() {
-	const router = useRouter();
-	const handleClick = () => {
-		router.replace('/Auth');
-	};
-
-	const navigation = useNavigation();
-	useFocusEffect(() => {
-		navigation.setOptions({
-			headerLeft: () => null,
-		});
-	});
-
 	return (
 		<>
 			<Stack.Screen
 				options={{
 					title: '',
-					headerShadowVisible: false,
+					headerShadowVisible: true,
 					headerShown: true,
 					headerBackVisible: false,
-					headerLeft: () => null,
-					gestureEnabled: true,
+					headerLeft: () => (
+						<s.HeaderButton>
+							<s.HeaderButtonText>로고 or 지역</s.HeaderButtonText>
+						</s.HeaderButton>
+					),
+					headerRight: () => (
+						<s.HeaderRightContainer>
+							<Pressable>
+								<Feather name='search' size={22} color='black' />
+							</Pressable>
+							<Pressable>
+								<Feather name='menu' size={22} color='black' />
+							</Pressable>
+						</s.HeaderRightContainer>
+					),
+					headerStyle: { backgroundColor: '#FFF' },
 					animation: 'none',
-					headerStyle: {
-						backgroundColor: '#FFF',
-					},
 				}}
 			/>
+
 			<s.Container>
-				<TouchableOpacity onPress={handleClick}>
-					<Text>Click Me</Text>
-				</TouchableOpacity>
-				<Text>Home</Text>
+				<FlatList
+					data={DATA}
+					keyExtractor={(_, index) => index.toString()}
+					showsVerticalScrollIndicator={false}
+					renderItem={({ item }) => (
+						<s.ItemContainer>
+							<Image
+								source={{ uri: item.image }}
+								style={{ width: 100, height: 100, borderRadius: 6 }}
+							/>
+							<s.ItemContent>
+								<s.Title>{item.title}</s.Title>
+								<s.SubInfo>
+									{item.location} · {item.date}
+								</s.SubInfo>
+								<s.Price>{item.price}</s.Price>
+								<s.Status>
+									<FontAwesome name='commenting-o' size={14} color='#888' />
+									<s.StatusText>{item.comments}</s.StatusText>
+									<FontAwesome
+										name='heart-o'
+										size={14}
+										color='#888'
+										style={{ marginLeft: 10 }}
+									/>
+									<s.StatusText>{item.likes}</s.StatusText>
+								</s.Status>
+							</s.ItemContent>
+						</s.ItemContainer>
+					)}
+					ItemSeparatorComponent={() => <s.Separator />}
+				/>
+
+				<s.FloatingButton>
+					<Feather name='plus' size={28} color='#fff' />
+				</s.FloatingButton>
 			</s.Container>
 		</>
 	);
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,11 +3,94 @@ import { font } from './font';
 import { theme } from './theme';
 
 export const Container = styled.View`
-  background-color: #fff;
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 16px;
+  background-color: #fff;
+  position: relative;
+  padding: 20px 24px;
+  margin-top: 4px;
+`;
+
+export const HeaderButton = styled.Pressable`
+  margin-left: 20px;
+`;
+
+export const HeaderButtonText = styled.Text`
+  font-size: 14px;
+  color: black;
+`;
+
+export const HeaderRightContainer = styled.View`
+  flex-direction: row;
+  gap: 16px;
+  margin-right: 18px;
+`;
+
+export const ItemContainer = styled.View`
+  flex-direction: row;
+  background-color: #fff;
+  padding-top: 16px;
+`;
+
+export const ItemContent = styled.View`
+  width: 100%;
+  margin-left: 20px;
+  display: flex;
   flex-direction: column;
   justify-content: space-between;
+  gap: 4px;
+  padding: 16px 0;
+`;
+
+export const Title = styled.Text`
+  ${font.Body2}
+  color: ${theme.Black};
+`;
+
+export const SubInfo = styled.Text`
+  ${font.Caption2}
+  color: ${theme.Gray4};
+  margin: 4px 0;
+`;
+
+export const Price = styled.Text`
+  ${font.Btn2}
+  color: ${theme.G400};
+`;
+
+export const Status = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3px;
+`;
+
+export const StatusText = styled.Text`
+  margin-left: 4px;
+  color: ${theme.Gray4};
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background-color: #eee;
+  margin: 0 16px;
+`;
+
+export const FloatingButton = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
+  background-color: ${theme.G400};
+  color: ${theme.White};
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+  justify-content: center;
   align-items: center;
 `;
