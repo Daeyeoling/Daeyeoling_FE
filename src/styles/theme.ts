c4f0d994dfd567712c2bdefe744af38aea0bf1a0
Jamkris <dltmdgus1412@gmail.com>
2025-04-10 14:58:18 +0900

    fix :: 테마 수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,16 +1,14 @@
-import { css } from 'styled-components/native';
-
 export const theme = {
-	G100: `#F5FFFC`,
-	G200: `#8DF7D4`,
-	G300: `#44E4AE`,
-	G400:`#1DBD88`,
-	G500: `#0E875F`,
-	Gray1: `#F9F9F9`,
-	Gray2: `#E5E5E5`,
-	Gray3: `#C3C3C3`,
-	Gray4: `#8C8C8C`,
-	Gray5: `#5E5E5E`,
-	White	: `#FFF`,
-	Black: `#222222`,
+	G100: '#F5FFFC',
+	G200: '#8DF7D4',
+	G300: '#44E4AE',
+	G400: '#1DBD88',
+	G500: '#0E875F',
+	Gray1: '#F9F9F9',
+	Gray2: '#E5E5E5',
+	Gray3: '#C3C3C3',
+	Gray4: '#8C8C8C',
+	Gray5: '#5E5E5E',
+	White: '#FFF',
+	Black: '#222222',
 };
